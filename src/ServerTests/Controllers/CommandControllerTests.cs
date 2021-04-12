d78abda289a243d7e3b1183471638206968246f1
GalaxyMom <galaxymomtwitch@gmail.com>
2021-04-11 20:36:11 -0700

    Added testing for CommandController

---
+++ src/ServerTests/Controllers/CommandControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenRP.Framework.Server.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenRP.Framework.Server.Controllers.Tests
+{
+    [TestClass()]
+    public class CommandControllerTests
+    {
+        [TestMethod()]
+        public void RegisterTest()
+        {
+            var command = new ServerCommand()
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UnregisterTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ tests/Server.Tests/Controllers/CommandControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace OpenRP.Framework.Server.Controllers.Tests
+{
+    public class CommandControllerTests
+    {
+        [Fact]
+        public void RegisterTest()
+        {
+            var CommandController = new CommandController(null);
+            CommandController.Register("test", new Action<int>(i => { }), "test", new List<string>());
+            Assert.Single(CommandController.Commands);
+            CommandController.Register("/test", new Action<int>(i => { }), "test", new List<string>());
+            Assert.Single(CommandController.Commands);
+        }
+
+        [Fact]
+        public void UnregisterTest()
+        {
+            var CommandController = new CommandController(null);
+            CommandController.Register("test", new Action<int>(i => { }), "test", new List<string>());
+            CommandController.Unregister("test");
+            Assert.Empty(CommandController.Commands);
+
+            CommandController = new CommandController(null);
+            CommandController.Unregister("test");
+            Assert.Empty(CommandController.Commands);
+        }
+    }
+}
