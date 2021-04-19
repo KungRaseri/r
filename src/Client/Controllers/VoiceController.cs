1860f040f32d51445a6347884f85e8e951ed0b03
GalaxyMom <galaxymomtwitch@gmail.com>
2021-04-19 14:21:00 -0700

    Updated data storage

--- src/Client/Controllers/VoiceController.cs
+++ src/Client/Controllers/VoiceController.cs
@@ -8,16 +8,24 @@ using System.Drawing;
 using System.Threading.Tasks;
 using CitizenFX.Core.UI;
 using OpenRP.Framework.Client.Classes;
+using System.Collections.Generic;
 
 namespace OpenRP.Framework.Client.Controllers
 {
     public class VoiceController : ClientAccessor
     {
+        int _grid;
         int _lastGrid;
+        List<int> _zones;
+        string _zonesList;
 
         public VoiceController(ClientMain client) : base(client)
         {
+            _grid = 0;
             _lastGrid = 0;
+            _zones = new List<int>();
+            _zonesList = "";
+
             MumbleSetAudioInputDistance(30f);
             MumbleSetAudioOutputDistance(30f);
             client.RegisterTickHandler(GameTick);
@@ -26,29 +34,30 @@ namespace OpenRP.Framework.Client.Controllers
         private async Task GameTick()
         {
             var pos = Game.PlayerPed.Position;
-            var zones = VoiceZone.GetZones(pos);
-            var grid = zones[0];
+            _zones = VoiceZone.GetZones(pos);
+            _grid = _zones[0];
             var temp = "";
-            var textGrid = new Text($"Grid: {grid} | {NetworkIsPlayerTalking(Game.Player.Handle)}", new PointF(1f, 1f), 0.5f);
-            var textNearby = new Text($"{temp}", new PointF(1.0f, 20.0f), 0.5f);
 
-            textGrid.Draw();
-            textNearby.Draw();
-
-            if (_lastGrid != grid)
+            if (_lastGrid != _grid)
             {
-                
                 MumbleClearVoiceTargetChannels(1);
-                NetworkSetVoiceChannel(grid);
+                NetworkSetVoiceChannel(_grid);
 
-                foreach (var zone in zones)
+                foreach (var zone in _zones)
                 {
                     temp += zone + " | ";
                     MumbleAddVoiceTargetChannel(1, zone);
                 }
 
-                _lastGrid = grid;
+                _zonesList = temp;
+                _lastGrid = _grid;
             }
+
+            var textGrid = new Text($"Grid: {_grid} | {NetworkIsPlayerTalking(Game.Player.Handle)}", new PointF(1f, 1f), 0.5f);
+            var textNearby = new Text($"{_zonesList}", new PointF(1.0f, 20.0f), 0.5f);
+
+            textGrid.Draw();
+            textNearby.Draw();
         }
     }
 }
