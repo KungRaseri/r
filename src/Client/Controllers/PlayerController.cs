343ca95b400bf37d3982c60b6c1f669beb2f8169
GalaxyMom <galaxymomtwitch@gmail.com>
2021-04-22 22:31:00 -0700

    Removed wanted levels Removed police spawns Removed dispatching of all emergency services

---
+++ src/Client/Controllers/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using CitizenFX.Core;
+using System.Threading.Tasks;
+
+namespace OpenRP.Framework.Client.Controllers
+{
+    public class PlayerController : ClientAccessor
+    {
+        internal PlayerController (ClientMain client) : base (client)
+        {
+            Game.Player.DispatchsCops = false;
+            Client.RegisterTickHandler(GameTick);
+        }
+
+        private async Task GameTick()
+        {
+            if (Game.Player.WantedLevel > 0)
+            {
+                Game.Player.WantedLevel = 0;
+            }
+
+            await BaseScript.Delay(50);
+        }
+    }
+}
