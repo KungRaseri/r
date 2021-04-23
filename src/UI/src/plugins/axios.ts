f5f335e3ccf55d0d4add30cb89f030f9249da1a5
KungRaseri <me@kungraseri.ninja>
2021-04-22 23:21:24 -0700

    hopefully clean up some of the code smells

--- src/UI/src/plugins/axios.ts
+++ src/UI/src/plugins/axios.ts
@@ -7,53 +7,53 @@ import axios from 'axios';
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 const config = {
-  // baseURL: process.env.baseURL || process.env.apiUrl || ""
-  // timeout: 60 * 1000, // Timeout
-  // withCredentials: true, // Check cross-site Access-Control
+    // baseURL: process.env.baseURL || process.env.apiUrl || ""
+    // timeout: 60 * 1000, // Timeout
+    // withCredentials: true, // Check cross-site Access-Control
 };
 
 const _axios = axios.create(config);
 
 _axios.interceptors.request.use(
-  (cfg: any) => {
-    // Do something before request is sent
-    return cfg;
-  },
-  (err: any) => {
-    // Do something with request error
-    return Promise.reject(err);
-  },
+    (cfg: any) => {
+        // Do something before request is sent
+        return cfg;
+    },
+    (err: any) => {
+        // Do something with request error
+        return Promise.reject(err);
+    }
 );
 
 // Add a response interceptor
 _axios.interceptors.response.use(
-  (res: any) => {
-    // Do something with response data
-    return res;
-  },
-  (err: any) => {
-    // Do something with response error
-    return Promise.reject(err);
-  },
+    (res: any) => {
+        // Do something with response data
+        return res;
+    },
+    (err: any) => {
+        // Do something with response error
+        return Promise.reject(err);
+    }
 );
 
-const Plugin: PluginObject<any> = {
-  install: (Vue: any) => {
-    Vue.$axios = _axios;
-  },
-};
-Plugin.install = (Vue: any) => {
-  Vue.$axios = _axios;
-  (window as any).axios = _axios;
-  Object.defineProperties(Vue.prototype, {
-    $axios: {
-      get() {
-        return _axios;
-      },
+const AxiosPlugin: PluginObject<any> = {
+    install: (VueApp: any) => {
+        VueApp.$axios = _axios;
     },
-  });
+};
+AxiosPlugin.install = (VueApp: any) => {
+    VueApp.$axios = _axios;
+    (window as any).axios = _axios;
+    Object.defineProperties(VueApp.prototype, {
+        $axios: {
+            get() {
+                return _axios;
+            },
+        },
+    });
 };
 
-Vue.use(Plugin);
+Vue.use(AxiosPlugin);
 
-export default Plugin;
+export default AxiosPlugin;
